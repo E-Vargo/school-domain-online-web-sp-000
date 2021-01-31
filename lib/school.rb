b4154383264747a46288e8d94f3881c24092c3a6
Elliott Vargo <vargoelliott@gmail.com>
2021-01-31 21:21:52 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+class School
+  attr_accessor
+  attr_reader :name, :roster
+
+def initialize(name)
+  @name = name
+  @roster = {}
+end
+
+def add_student(new_student, grade)
+  if  @roster == {}
+      @roster[grade] = []
+      @roster[grade] << new_student
+  elsif @roster.keys.find {|k| k == grade}
+        @roster[grade] << new_student
+  elsif @roster.keys.find {|k| k != grade}
+      @roster[grade] = []
+      @roster[grade] << new_student
+    end
+end
+
+#def grade(num)
+#@roster[num]
+#end
+
+def sort
+if
+end
+
+
+
+end
