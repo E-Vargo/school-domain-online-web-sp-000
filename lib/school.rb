25128a814f023fbf387fe4df299646a3336ce5e5
Elliott Vargo <vargoelliott@gmail.com>
2021-01-31 21:46:27 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,11 +12,8 @@ def add_student(new_student, grade)
   if  @roster == {}
       @roster[grade] = []
       @roster[grade] << new_student
-  elsif @roster.keys.find {|k| k == grade}
-        @roster[grade] << new_student
-  elsif @roster.keys.find {|k| k != grade}
-      @roster[grade] = []
-      @roster[grade] << new_student
+  else
+    @roster[grade] = [new_student]
     end
 end
 
